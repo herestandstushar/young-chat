596ea911c848357dd6fa5d7e21762b21a0ad1699
unknown <tomertusharthakur@gmail.com>
2020-07-05 18:09:14 +0530

    init

--- src/index.js
+++ src/index.js
@@ -60,6 +60,6 @@ io.on('connection',(socket)=>{
 
 })
  const port= process.env.PORT || 3000
-server.listen(3000,()=>{
-    console.log('server started at port 3000!')
+server.listen(port,()=>{
+    console.log('server started')
 })
